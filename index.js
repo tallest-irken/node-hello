327b85dab77c712dfb00a2bf876c47a11580a9bf
tallest-irken <145706120+tallest-irken@users.noreply.github.com>
2023-11-14 18:45:05 +0000

    another test change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! A trivial change.\n'
+  const msg = 'Hello Node! Another trivial change.\n'
   res.end(msg);
 });
 
